77d2a6e3eedfce4cbc4e4aaafa992933173a9efd
Charles Yap Yaocheng <charles_yap_yc@aiap.sg>
2024-05-14 00:22:42 +0800

    edited conversion to alllow for empty df columns

--- conversion.py
+++ conversion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import numpy as np  # Import numpy to handle NaN
 
 # Load JSON data from file
 with open('songs_checkpoint.json', 'r') as file:
@@ -11,5 +12,12 @@ songs_df.index.name = 'song_name'  # Adding an index name if you want to include
 
 # Rename the 'user' column to 'user_full_name'
 songs_df.rename(columns={'user': 'user_full_name'}, inplace=True)
+
+# Initialize new columns with NaN or None
+songs_df['priority_number'] = np.nan  # Assuming priority number is a numeric field
+songs_df['matched_song_name'] = pd.NA  # For string fields, using pandas NA
+songs_df['genre'] = pd.NA
+songs_df['artist'] = pd.NA
+
 # Save the DataFrame to a CSV file
 songs_df.to_csv('songs.csv')
